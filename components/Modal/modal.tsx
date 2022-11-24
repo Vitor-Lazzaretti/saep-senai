b37e7be870f97290efd745ebd7efdbfa8ab4eaf1
VitorLazzaretti <vitorlazzaretti@gmail.com>
2022-11-24 11:41:19 -0300

    Update modal.tsx

--- components/Modal/modal.tsx
+++ components/Modal/modal.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 import style from './Modal.module.css'
 export default function Modal({children}: any) {
     return(
         <div className={style.modal_container}>
             <div className={style.modal}>
                 <div className={style.modal_head_container}>
-                    <img src="/x.svg" alt="X" />
+                    <Link href={'/'}>
+                        <img src="/x.svg" alt="X" />
+                    </Link>
                 </div>
                 <div className={style.content_modal}>
                     {children}
